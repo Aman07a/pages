39d0ea1adbc51be4cbc11033756abb7627f06c27
Aman07a <aman16.aa17@gmail.com>
2024-01-24 11:10:59 +0100

    Custom Structural Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ClassDirective } from './class.directive';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
-  declarations: [AppComponent, ClassDirective],
+  declarations: [AppComponent, ClassDirective, TimesDirective],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
